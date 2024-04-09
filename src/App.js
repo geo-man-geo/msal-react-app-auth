574f11ef6e8be074453cd745676dfe626f4afd3d
Geo Manuel George <geomanforpro@gmail.com>
2024-04-09 22:27:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  AuthenticatedTemplate,
+  MsalProvider,
+  UnauthenticatedTemplate,
+  useMsal,
+} from "@azure/msal-react";
+import { loginRequest } from "./auth-config";
+import "./App.css";
 
-function App() {
+const WrappedView = () => {
+  const { instance } = useMsal();
+  const activeAccount = instance.getActiveAccount();
+
+  const handleRedirect = () => {
+    instance
+      .loginRedirect({
+        ...loginRequest,
+        promt: "create",
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AuthenticatedTemplate>
+        {activeAccount ? <p>This is a sample BOB webapp authenticated using MSAL</p> : null}
+      </AuthenticatedTemplate>
+      <UnauthenticatedTemplate>
+        <button onClick={handleRedirect}>Sign Up</button>
+      </UnauthenticatedTemplate>
     </div>
   );
-}
+};
+
+const App = ({ instance }) => {
+  return (
+    <MsalProvider instance={instance}>
+      <WrappedView />
+    </MsalProvider>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {PublicClientApplication, EventType} from '@azure/msal-browser'
+import { msalConfig } from './auth-config';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const msalInstance = new PublicClientApplication(msalConfig);
+
+if(!msalInstance.getActiveAccount() && msalInstance.getAllAccounts().length > 0){
+  msalInstance.setActiveAccount(msalInstance.getActiveAccount()[0]);
+}
+
+msalInstance.addEventCallback((event) => {
+  if(event.eventType === EventType.LOGIN_SUCCESS && event.payload.account){
+    const account = event.payload.account;
+    msalInstance.setActiveAccount(account);
+  }
+})
 root.render(
   <React.StrictMode>
-    <App />
+    <App instance={msalInstance} />
   </React.StrictMode>
 );
 
